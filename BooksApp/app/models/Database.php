fb75b1d68645fe7258b4dc2354fac4376f20c1a3
Dalibor Topinka <dalibor.topinka@tul.cz>
2026-04-02 11:30:48 +0200

    funkci

--- BooksApp/app/models/Database.php
+++ BooksApp/app/models/Database.php
@@ -33,7 +33,7 @@ class Database {
 
 // Pro otestování připojení stačí tento soubor spustit
 // Můžete tento kód zakomentovat po ověření
-//$database = new Database();
+$database = new Database();
 
 // na objektu uloženém v proměnné $database zavoláme metodu getConnection()
-//$database->getConnection();
+$database->getConnection();
