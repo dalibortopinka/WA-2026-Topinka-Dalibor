3f2989abb15eab39b240c7b6892f9b0dd2c1ef25
Dalibor Topinka <dalibor.topinka@tul.cz>
2026-04-02 11:42:17 +0200

    pohoda

--- BooksApp/app/views/books/books_list.php
+++ BooksApp/app/views/books/books_list.php
@@ -16,8 +16,8 @@
                 <!-- Poznámka: do odkazu href se musí dát reálná absolutní/úplná cesta k souboru index.php -->
                 <nav>
     <ul>
-        <li><a href="/WA-2026-Topinka-Dalibor/BooksApp/public/index.php">Seznam knih (Domů)</a></li>
-        <li><a href="/WA-2026-Topinka-Dalibor/BooksApp/public/index.php?url=book/create">Přidat novou knihu</a></li>
+        <li><a href="<?= BASE_URL ?>/index.php">Seznam knih (Domů)</a></li>
+        <li><a href="<?= BASE_URL ?>/index.php?url=book/create">Přidat novou knihu</a></li>
     </ul>
 </nav>
             </ul>
